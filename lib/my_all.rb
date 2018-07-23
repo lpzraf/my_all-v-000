423b12d70d468270c1ac287cd83d78106df6a8b2
Rafael Lopez <lpz.raf@gmail.com>
2018-07-23 01:23:42 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,13 +1,12 @@
-require 'pry'
 
 def my_all?(collection)
-  i = 0 
+  i = 0
   block_return_values = []
   while i < collection.length
-  block_return_values << yield(collection[1])
-    i += 1
-  end  
-  
+    block_return_values << yield(collection[i])
+    i = i + 1
+  end
+ 
   if block_return_values.include?(false)
     false
   else
